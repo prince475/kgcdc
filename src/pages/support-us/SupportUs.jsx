a266becf03256e0c86fbb9c911779508c78f87cd
abdishukri mohamed <abdishukrimohamed105@gmail.com>
2024-08-03 02:49:02 +0300

    setep support page

--- src/pages/support-us/SupportUs.jsx
+++ src/pages/support-us/SupportUs.jsx
@@ -1,5 +1,18 @@
+import JoinUs from "./sections/JoinUs";
+import OurPatners from "./sections/OurPatners";
+import SectionHeading from "@/components/texts/SectionHeading";
 const SupportUs = () => {
-  return <div>SupportUs</div>;
+  return (
+
+    <section >
+       <SectionHeading className=" font-montserrat text-left text-purple-800 px-3 lg:py-10 lg:px-24">
+        Our Patners
+      </SectionHeading>
+
+      <OurPatners />
+      <JoinUs />
+    </section>
+  )
 };
 
 export default SupportUs;
